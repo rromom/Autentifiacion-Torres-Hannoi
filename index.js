99f4e7a8fb67e7749f4c37f58267b7815e878f87
rromom <rromom@est.ups.edu.ec>
2022-04-06 16:44:01 -0500

    cambios realizados

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ const morgan = require('morgan');
 const session = require('express-session');
 const flash = require('connect-flash');
 const handlebars = require('express-handlebars');
+const csrf = require('csurf');
+
+
 const app = express();
 
-
 env.config();
+
 app.use(morgan('dev'));
-app.use(session({ secret: process.env.SECRET, resave: true, saveUninitialized: true }))
+app.use(session({
+    secret: process.env.SECRET,
+    //secret: 'super-secret-key',
+    resave: true,
+    saveUninitialized: true,
+    //cookie:false 
+}))
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(flash())
@@ -36,4 +45,6 @@ app.use(require('./server/routes/routes-index'))
 
 
 
-app.listen(process.env.PORT, () => console.log(`http://localhost:${process.env.PORT}`));
+app.listen(process.env.PORT,() => {
+    console.log('http://localhost:'+process.env.PORT);
+});
--- server/routes/routes-index.js
+++ server/routes/routes-index.js
@@ -4,6 +4,14 @@ const pool = require('../database')
 
 const router = express.Router();
 
+const middleware = (req,res,next) =>{
+    if (!req.session.user) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+}
+
 router.get('/', (req, res) => {
     res.render('main', { layout: 'index',active1:'active'});
 });
@@ -22,6 +30,7 @@ router.get('/servicios',(req,res)=>{
 
 router.get('/login', (req, res) => {
     res.render('login', { layout: 'index', error: req.flash('error') });
+
 });
 
 router.post('/ingreso', async(req, res) => {
@@ -32,21 +41,47 @@ router.post('/ingreso', async(req, res) => {
 
     let sql = 'SELECT * FROM `usuario` WHERE `email_user`="' + email + '" AND `password_user`="' + pwd+'"';
     let result = await pool.query(sql);
-    
-    let texto;
 
-    console.log(result.length);
     if (result.length == 1) {
         texto = "BIENVENIDO";
-        res.render(`sesion`,{layout:'dashboard',texto,nick:result[0].nick_user});
+        req.session.user = result;
+        res.redirect('/sesion');
      } else {
          let errores={};
          errores.credencial='Credenciales no validas';
          res.render('login',{layout:'index',errores});
      }
 });
-router.get('/sesion', (req, res) => {
-    res.render('sesion', { layout: 'dashboard', texto: req.query.text, user: req.query.user });
+
+router.get('/sesion',middleware,(req, res) => {
+    console.log(req.session.user)
+    res.render('sesion', { layout: 'dashboard', texto: "BIENVENIDO", nick: req.session.user[0].nick_user, saldo:req.session.user[0].saldo_usuario, email:req.session.user[0].email_user });
+})
+
+
+router.post('/edit', middleware,async(req,res) => {
+    let sql = "UPDATE usuario set saldo_usuario=? WHERE email_user=?"
+    const result = await pool.query(sql,[req.body.saldo,`${req.session.user[0].email_user}`]);
+    let sql1 = 'SELECT * FROM `usuario` WHERE `email_user`=?';
+    const result1 = await pool.query(sql1,[`${req.session.user[0].email_user}`]);
+    console.log(result1)
+    req.session.user = result1;
+    res.send("Actualizado")
+})
+
+
+router.get('/email',middleware,(req, res) => {
+    console.log(req.session.user)
+    res.render('email', { layout: 'dashboard'});
+})
+
+router.post('/edit/email',middleware, async(req , res) => {
+    const {email}= req.body;
+    let sql = "UPDATE usuario set email_user=? WHERE email_user=?"
+    const result = await pool.query(sql,[ `${email}`, req.session.user[0].email_user]);
+    req.session.user[0].email_user = email;
+    req.session.destroy();
+    res.redirect('/sesion');
 })
 
 router.get('/register', async(req, res) => {
@@ -55,7 +90,7 @@ router.get('/register', async(req, res) => {
 
 router.post('/register', async(req = request, res = response) => {
     let {nickname,nombre,apellido,email,psw} = req.body;
-    //let sql = "SELECT  `email_user`, `nick_user` FROM `usuario` Where email_user='"+email+"' OR nick_user='"+nickname+"'";
+    // let sql = "SELECT  `email_user`, `nick_user` FROM `usuario` Where email_user='"+email+"' OR nick_user='"+nickname+"'";
     let sql = "SELECT  `email_user`, `nick_user` FROM `usuario` Where `email_user`=? OR `nick_user`=?";
     const result = await pool.query(sql,[`${email}`,`${nickname}`]);
     let errores = {}
@@ -73,8 +108,8 @@ router.post('/register', async(req = request, res = response) => {
     if (Object.keys(errores).length === 0) {
         //let insert = 'INSERT INTO `usuario`(`email_user`, `nick_user`,  `names_user`, `last_user`, `password_user`) VALUES ' +
             //'("' + email + '","' +nickname + '","' +nombre + '","' +apellido+ '","' +pwd + '")';
-            let insert = 'INSERT INTO `usuario`(`email_user`, `nick_user`,  `names_user`, `last_user`, `password_user`) VALUES  ( ? , ? , ? , ? , ? )';
-        await pool.query(insert, [`${email}`,`${nickname}`,`${nombre}`,`${apellido}`,`${psw}`]);
+        let insert = 'INSERT INTO `usuario`(`email_user`, `nick_user`,  `names_user`, `last_user`, `password_user`,`saldo_usuario`) VALUES  ( ? , ? , ? , ? , ? ,?)';
+        await pool.query(insert, [`${email}`,`${nickname}`,`${nombre}`,`${apellido}`,`${psw}`,`100`]);
         res.redirect('/login');
     } else {
         res.render('register',{layout:'index',errores:errores,nombre,apellido});
